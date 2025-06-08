364a37f99974369b8ca35167c54fc5dc800a0873
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-06-08 15:45:54 -0300

    url railway

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,9 +1,8 @@
 const isProduction = typeof window !== "undefined" && window.location.hostname !== "localhost";
 
 export const API_BASE = isProduction
-    ? "https://ccr-alertas-api-production.up.railway.app"
+    ? "https://globaljava-production.up.railway.app"
     : "http://localhost:8080";
-
 
 export const getHeaders = () => ({
     "Content-Type": "application/json",
